5e2429b40595415fd64b7c7a7d921d7d723e3048
jmh <120222392@qq.com>
2016-06-15 17:10:01 +0800

    什么都没修改...

--- app/src/main/java/com/example/administrator/news/MainActivity.java
+++ app/src/main/java/com/example/administrator/news/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerAdapter mAdapter;
     private List<NewsBean> mDatas;
-    private DisplayImageOptions options;
+    public DisplayImageOptions options;
     private String[] images;
 
     @Override
@@ -57,22 +57,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void initDatas() {
         NewsBean mNews = new NewsBean();
-        Bundle bundle = getIntent().getExtras();
-        images = bundle.getStringArray(Constants.IMAGES);
-        options = new DisplayImageOptions.Builder()
-                .showImageOnLoading(R.mipmap.ic_launcher) // 设置图片下载期间显示的图片
-                .showImageForEmptyUri(R.mipmap.ic_launcher) // 设置图片Uri为空或是错误的时候显示的图片
-                .showImageOnFail(R.mipmap.ic_launcher) // 设置图片加载或解码过程中发生错误显示的图片
-                .resetViewBeforeLoading(false)  // default 设置图片在加载前是否重置、复位
-                .delayBeforeLoading(1000)  // 下载前的延迟时间
-                .cacheInMemory(true) // default  设置下载的图片是否缓存在内存中
-                .cacheOnDisk(true) // default  设置下载的图片是否缓存在SD卡中
 
-                .imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2) // default 设置图片以如何的编码方式显示
-                .bitmapConfig(Bitmap.Config.ARGB_8888) // default 设置图片的解码类型
-                .displayer(new SimpleBitmapDisplayer()) // default  还可以设置圆角图片new RoundedBitmapDisplayer(20)
-                .handler(new Handler()) // default
-                .build();
+        images = Constants.images;
+
         ImageLoader imageLoader = ImageLoader.getInstance();
         NetApi.getNewsList(new ICallback() {
             @Override
--- app/src/main/java/com/example/administrator/news/present/RecyclerAdapter.java
+++ app/src/main/java/com/example/administrator/news/present/RecyclerAdapter.java
@@ -1,5 +1,7 @@
 package com.example.administrator.news.present;
 
+import android.graphics.Bitmap;
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,8 @@ import com.example.administrator.news.model.NewsBean;
 import com.example.administrator.news.net.NetApi;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.SimpleBitmapDisplayer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,6 +52,20 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         if (holder instanceof MyViewHolder){
             MyViewHolder holder1= (MyViewHolder)holder;
           //  int newsImage=Integer.parseInt(news.getPic_url());
+            options = new DisplayImageOptions.Builder()
+                    .showImageOnLoading(R.mipmap.ic_launcher) // 设置图片下载期间显示的图片
+                    .showImageForEmptyUri(R.mipmap.ic_launcher) // 设置图片Uri为空或是错误的时候显示的图片
+                    .showImageOnFail(R.mipmap.ic_launcher) // 设置图片加载或解码过程中发生错误显示的图片
+                    .resetViewBeforeLoading(false)  // default 设置图片在加载前是否重置、复位
+                    .delayBeforeLoading(1000)  // 下载前的延迟时间
+                    .cacheInMemory(true) // default  设置下载的图片是否缓存在内存中
+                    .cacheOnDisk(true) // default  设置下载的图片是否缓存在SD卡中
+
+                    .imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2) // default 设置图片以如何的编码方式显示
+                    .bitmapConfig(Bitmap.Config.ARGB_8888) // default 设置图片的解码类型
+                    .displayer(new SimpleBitmapDisplayer()) // default  还可以设置圆角图片new RoundedBitmapDisplayer(20)
+                    .handler(new Handler()) // default
+                    .build();
 
             mImageLoader.displayImage(news.getPic_url(),holder1.newsImage,options);
 
